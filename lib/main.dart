27fdfa1161dd134db91848972de0a4a60a4bf833
Volha Zinkevich <o.zinkevich@icloud.com>
2023-08-29 15:54:05 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() => runApp(
+  MaterialApp(
+    home: BallPage(),
+  ),
+);
+
+class Ball extends StatefulWidget {
+  const Ball({super.key});
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: TextButton(
+            onPressed: () {
+              setState(() {
+                ballNumber = Random().nextInt(5) + 1;
+                print(ballNumber);
+              });
+            },
+            child: Image.asset('images/ball$ballNumber.png'),
+        )
+    );
+  }
+}
+
+
+class BallPage extends StatelessWidget {
+  const BallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.indigoAccent[50],
+      appBar: AppBar(
+        backgroundColor: Colors.indigoAccent,
+        title: Text('Ask me anything!'),
+      ),
+      body: Ball(),
+    );
+  }
+}
